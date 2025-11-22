4f272006ae9eb9c2d63d86d09549fb159874adb0
Dimitri <sivomnii@gmail.com>
2025-11-22 16:23:46 -0500

    Fixed multiplication.py -remove quote marks from por proper syntax

--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,5 @@
-"# Multiplication Functions" 
-"def multiply(a, b):" 
-"    return a * b" 
-"" 
-"print('5 * 7 =', multiply(5, 7))" 
+# Multiplication Functions 
+def multiply(a, b): 
+    return a * b 
+ 
+print('5 * 7 =', multiply(5, 7)) 
